4d9ac1a38d9a64d1dbc5231a538883fc7f730dec
Thomas Klein <thomas.klein@jwalab.com>
2021-11-15 13:25:39 +0100

    fixed di config for docs handler

--- packages/nats-runner/src/di.ts
+++ packages/nats-runner/src/di.ts
@@ -2,6 +2,7 @@ module.exports = {
     services: {
         GetDocsHandler: {
             class: "openApi",
+            main: "GetDocsHandler",
             tags: [{ name: "nats.handler" }],
             arguments: ["%cwd%", "%config.SERVICE_NAME%", "@logger"]
         },
